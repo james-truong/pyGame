d227400cc65b207019459581d722f9291db0a98f
James Truong <james.truong@hotmail.com>
2020-02-26 16:39:52 -0500

    added jump quadratic formula

--- firstGame.py
+++ firstGame.py
@@ -5,12 +5,15 @@ win = pygame.display.set_mode((750,750))
 
 pygame.display.set_caption("First Game")
 
-x = 50
-y = 50
+x = 375
+y = 375
 
 width = 40
 height = 60
 vel = 10
+
+isJump = False
+jumpCount = 10
 
 run = True
 while run:
@@ -22,14 +25,28 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 750 - width:
         x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
+    if not(isJump):
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < 750 - height:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+        
     win.fill((0,0,0))
     pygame.draw.rect(win, (255, 0, 0), (x,y, width, height))
     pygame.display.update()
